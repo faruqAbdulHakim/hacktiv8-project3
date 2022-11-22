ea06a0ea66f5909b0e957b07ef004e5540c80d95
perdanaph <danangputro9@gmail.com>
2022-11-22 21:01:16 +0700

    refactoring: tidy up code

--- controllers/TransactionHistoryController.js
+++ controllers/TransactionHistoryController.js
@@ -3,9 +3,9 @@ const { toRupiah } = require('../helpers/currencyHelper');
 
 const TransactionHistoryController = {
   create: async (req, res, next) => {
-    const user = req.user;
-    const { productId, quantity } = req.body;
     try {
+      const user = req.user;
+      const { productId, quantity } = req.body;
       if (!productId || !quantity) {
         return res.status(400).json({ message: 'Data tidak boleh kosong' });
       }
@@ -13,13 +13,15 @@ const TransactionHistoryController = {
       const data = await Product.findOne({
         where: { id: productId },
       });
+
       const customer = await User.findOne({
         where: { id: user.id },
       });
-      console.log(customer.dataValues.balance);
+
       if (!data) {
         return res.status(404).json({ message: 'product does not exist' });
       }
+
       if (data.dataValues.stock < quantity) {
         return res.status(400).json({
           quantity,
@@ -27,6 +29,7 @@ const TransactionHistoryController = {
           message: `Stok tidak cukup untuk memproses pembelian anda`,
         });
       }
+
       const total_price = quantity * data.dataValues.price;
       if (customer.balance < total_price) {
         return res.status(400).json({
@@ -36,6 +39,7 @@ const TransactionHistoryController = {
           message: 'Saldo tidak mencukupi untuk melakukan transaksi',
         });
       }
+
       await Product.update(
         {
           stock: data.dataValues.stock - quantity,
@@ -44,6 +48,7 @@ const TransactionHistoryController = {
           where: { id: productId },
         }
       );
+
       await User.update(
         {
           balance: customer.dataValues.balance - total_price,
@@ -55,6 +60,7 @@ const TransactionHistoryController = {
           hooks: false,
         }
       );
+
       const sold_product_amount = await Category.findOne({ where: { id: data.dataValues.CategoryId } });
       await Category.update(
         {
@@ -62,6 +68,7 @@ const TransactionHistoryController = {
         },
         { where: { id: data.dataValues.CategoryId } }
       );
+
       await TransactionHistory.create({
         ProductId: productId,
         UserId: user.id,
@@ -77,7 +84,7 @@ const TransactionHistoryController = {
         },
       });
     } catch (error) {
-      console.log(error);
+      next(error);
     }
   },
 
@@ -95,10 +102,12 @@ const TransactionHistoryController = {
           ],
           where: { UserId: user.id },
         });
+
         transaction.filter((column) => {
           column.dataValues.total_price = toRupiah(column.dataValues.total_price);
           column.dataValues.Product.price = toRupiah(column.dataValues.Product.price);
         });
+
         res.status(200).json({
           transactionHistories: transaction,
         });
@@ -125,6 +134,7 @@ const TransactionHistoryController = {
             },
           ],
         });
+
         transaction.filter((column) => {
           column.dataValues.total_price = toRupiah(column.dataValues.total_price);
           column.dataValues.Product.price = toRupiah(column.dataValues.Product.price);
@@ -160,12 +170,13 @@ const TransactionHistoryController = {
             message: 'Transaction not found',
           });
         }
+
         transaction.dataValues.total_price = toRupiah(transaction.dataValues.total_price);
         transaction.dataValues.Product.price = toRupiah(transaction.dataValues.Product.price);
         res.status(200).json(transaction);
       }
     } catch (error) {
-      console.log(error);
+      next(error);
     }
   },
 };
